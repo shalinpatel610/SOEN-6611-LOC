6767fcbc59116c919dbe489c36be468c3a18c5c5
Shalin Patel <shalinpatel610@gmail.com>
2020-02-15 16:57:08 -0500

    java test folder added

---
+++ JavaTestFolder/HelloWorld.java
@@ -0,0 +1,16 @@
+/* HelloWorld.java'
+ *
+ *
+ *
+ * Finish comment
+ */
+
+/*
+hello*/
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
